e2e6b2e2bff87f4fd26a58b01bb0529728e5cbdd
iDavi <69827032+iDavi@users.noreply.github.com>
2021-06-24 14:35:44 -0300

    Done :)

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Simple-Mudae-Bot
 
-selfbot simples pra claimar personagens da Mudae automaticamente.
+Selfbot simples pra claimar personagens da Mudae automaticamente.
 
-- selfbots são contra o ToS do Discord e esse projeto é apenas um teste. USE AT YOUR OWN RISK.
+- Selfbots são contra o ToS do Discord e esse projeto é apenas um teste. USE AT YOUR OWN RISK.
+
+- Para usar coloque seu token no .env como DISCORDTOKEN e escolha os personagens em personagens.json
--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 const Discord = require("discord.js-selfbot");
+const client = new Discord.Client();
+const fs = require("fs");
+
+client.on("ready", () => {
+  console.log("estou vivo");
+});
+
+client.on("message", (msg) => {
+  if (msg.author.id !== "432610292342587392") return;
+  if (
+    msg.embeds[0] === undefined ||
+    msg.embeds[0].author === undefined ||
+    msg.embeds[0].author.name === undefined
+  )
+    return;
+  const nomeDoPersonagem = msg.embeds[0].author.name;
+  const personagens = JSON.parse(fs.readFileSync("./src/personagens.json"));
+  personagens.forEach((item, index) => {
+    if (nomeDoPersonagem.includes(item)) {
+      msg.react("😄");
+    }
+  });
+});
+
+client.login(process.env.DISCORDTOKEN);
